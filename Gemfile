2eb32d33218d05e9b8dcc75e04aa2d1be5b7fd76
Ashanti Hatchett <ahatchett02@gmail.com>
2024-11-22 21:46:01 +0000

    added pundit

--- Gemfile
+++ Gemfile
@@ -85,6 +85,7 @@ gem "sqlite3", "~> 1.4"
 gem "table_print"
 gem "simple_form"
 gem "carrierwave"
+gem "pundit", "~> 2.4"
 
 group :development do
   gem "annotate"
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,26 @@
 class ApplicationController < ActionController::Base
+  include Pundit
+
   skip_forgery_protection
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  after_action :verify_authorized, except: :index
+  after_action :verify_policy_scoped, only: :index
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, :keys => [:username, :first_name, :last_name])
 
     devise_parameter_sanitizer.permit(:account_update, :keys => [:first_name, :last_name])
+  end
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    
+    redirect_back fallback_location: root_url
   end
 end
