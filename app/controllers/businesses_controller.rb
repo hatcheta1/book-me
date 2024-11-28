1dae8c4b77334719b69290844d5d4e563a48ac08
Ashanti Hatchett <ahatchett02@gmail.com>
2024-11-28 04:26:59 +0000

    added calendar with simple_calendar gem

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -61,6 +61,9 @@ class BusinessesController < ApplicationController
     end
   end
 
+  def calendar
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_business
