efd9519c99640432e64510d8287d8387ca429266
Ashanti Hatchett <ahatchett02@gmail.com>
2024-12-01 23:06:04 +0000

    changed default timezone to UTC

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,9 @@ module RailsTemplate
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "UTC"
+    config.active_record.default_timezone = :utc
+  
     # config.eager_load_paths << Rails.root.join("extras")
     config.generators do |g|
       g.test_framework nil
