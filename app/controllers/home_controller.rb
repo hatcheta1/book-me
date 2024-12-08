2de13f9147a14acb60ee94a7d1b779e2d31e8415
Ashanti Hatchett <ahatchett02@gmail.com>
2024-12-08 23:56:21 +0000

    skip scope for home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   skip_after_action :verify_authorized 
+  skip_after_action :verify_policy_scoped 
 
   def redirect_root
     if current_user&.businesses&.present?
