3d611cd35ea9bc41bffc05ef1cd5a30d89817625
Ashanti Hatchett <ahatchett02@gmail.com>
2024-12-08 20:38:52 +0000

    skip verify_authorized for home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  skip_after_action :verify_authorized 
+
   def redirect_root
     if current_user&.businesses&.present?
       redirect_to business_path(current_user.businesses.first)
