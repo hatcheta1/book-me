d017a60bdf6629f29cfbf772db3b8f95f0776b1f
Ashanti Hatchett <ahatchett02@gmail.com>
2024-12-08 19:34:55 +0000

    add users routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   resources :business_hours
   resources :services
   resources :search, only: [:index]
+  resources :users, only: [:index]
 
   get "/businesses/:business_name/bookings", to: "bookings#index_for_business", as: :business_bookings
   get "/users/:username/bookings",to: "bookings#index_for_client", as: :client_bookings
