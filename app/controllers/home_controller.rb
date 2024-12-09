799ee4e602f5d3157bd272929185ff64a5c64ec8
Ashanti Hatchett <ahatchett02@gmail.com>
2024-12-09 06:21:23 +0000

    added landing page with videos and separated it from business index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,14 @@
 class HomeController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def redirect_root
     if current_user&.businesses&.present?
       redirect_to business_path(current_user.businesses.first)
     else
-      redirect_to businesses_path
+      redirect_to home_index_path
     end
+  end
+
+  def index
   end
 end
