96733dbe0ce90dc950f1ebdf1ec7a20826aaf002
Ashanti Hatchett <ahatchett02@gmail.com>
2024-12-11 10:48:25 +0000

    skipped authorizations for search and pwa

--- app/controllers/pwa_controller.rb
+++ app/controllers/pwa_controller.rb
@@ -1,3 +1,7 @@
 class PwaController < ActionController::Base
+  skip_before_action :authenticate_user!
+  skip_after_action :verify_authorized 
+  skip_after_action :verify_policy_scoped 
+  
   def manifest; end
 end
