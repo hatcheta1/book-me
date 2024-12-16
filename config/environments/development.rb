c4557cb2d671bfc158c4deba0ec3bce2026cd7ae
Ashanti Hatchett <ahatchett02@gmail.com>
2024-12-16 04:00:16 +0000

    added bullet and mini-profiler gems

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,15 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.after_initialize do
+    Bullet.enable        = true
+    Bullet.alert         = true
+    Bullet.bullet_logger = true
+    Bullet.console       = true
+    Bullet.rails_logger  = true
+    Bullet.add_footer    = true
+  end
+  
   routes.default_url_options[:host] = 'https://animated-guacamole-jjw45rggxj52prqj-3000.app.github.dev/'
   # Allow server to be hosted on any URL
   config.hosts.clear
