8d36acd783e5053f11c3c31aeb252a7b18c1d13f
Ashanti Hatchett <ahatchett02@gmail.com>
2024-12-16 04:00:46 +0000

    added includes method to addres N+1 queries

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -80,7 +80,7 @@ class ServicesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_service
-      @service = Service.find(params[:id])
+      @service = Service.includes(:business, photo_attachment: :blob).find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
